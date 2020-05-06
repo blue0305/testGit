84070d3cae8e9f654d016782b7bf76c6e180075b
LCB <583491839@qq.com>
2020-05-06 20:50:59 +0800

    add User.java

---
+++ src/main/java/com/lcb/bean/User.java
@@ -0,0 +1,49 @@
+package com.lcb.bean;
+
+public class User {
+    private String name;
+    private String password;
+    private Integer age;
+    private String tel;
+
+    public User(String name, String password, Integer age, String tel) {
+        this.name = name;
+        this.password = password;
+        this.age = age;
+        this.tel = tel;
+    }
+
+    public User() { }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+}
