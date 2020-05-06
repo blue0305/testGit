4c5bd4a852746f8c2d0a23c71cdd1494ca90730e
LCB <583491839@qq.com>
2020-05-06 23:03:10 +0800

    add more print

--- src/main/java/com/lcb/test/Test2.java
+++ src/main/java/com/lcb/test/Test2.java
@@ -1,4 +1,16 @@
 package com.lcb.test;
 
 public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaddd");
+    }
 }
